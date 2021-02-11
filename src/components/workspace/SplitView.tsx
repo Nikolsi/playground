26336ae299a73aa460e74c1eae4c675b2d034752
Nikolsi <nikolai.a.arsenov@hotmail.com>
2021-02-11 18:58:17 +0100

    Setup CI

--- src/components/workspace/SplitView.tsx
+++ src/components/workspace/SplitView.tsx
@@ -2,13 +2,13 @@ import React from 'react'
 import SplitPane from 'react-split-pane'
 
 interface SplitViewProps {
-    children?: React.ReactNode
+  children?: React.ReactNode
 }
 
 export const SplitView = ({ children }: SplitViewProps) => {
-    return (
-        <SplitPane className='split-pane-container' split='vertical' defaultSize='50%' primary='first' minSize={200}>
-            {children}
-        </SplitPane>
-    )
+  return (
+    <SplitPane className='split-pane-container' split='vertical' defaultSize='50%' primary='first' minSize={200}>
+      {children}
+    </SplitPane>
+  )
 }
